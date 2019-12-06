a91b02f7788c4f484d9807b06eeff5dde7c7ec8f
hello <sereginalin@yandex.ru>
2019-12-06 15:16:03 +0300

    first version

---
+++ main.py
@@ -0,0 +1,11 @@
+def my_superfunction():
+    print('Print')
+
+
+def main():
+    print('My first git program.')
+    print('And I change it every day.')
+
+
+if __name__ == "__main__":
+    main()
